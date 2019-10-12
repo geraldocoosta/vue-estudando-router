5e3283e63f54fd936ca86b2724e7f892926e43e8
Geraldo Costa <geraldocoosta@gmail.com>
2019-10-12 11:02:40 -0300

    isso ae

---
+++ src/components/template/Menu.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav class="menu">
+    <ul>
+      <router-link to="/" tag="li" active-class="active" exact>
+        <a>Inicio</a>
+      </router-link>
+      <router-link to="/usuario" tag="li" active-class="active">
+        <a>Usuario</a>
+      </router-link>
+    </ul>
+  </nav>
+</template>
--- src/router.js
+++ src/router.js
@@ -14,7 +14,7 @@ export default new Router({
   routes: [
     {
       path: "/",
-      component: Inicio
+      component: Inicio,
     },
     {
       path: "/usuario",
